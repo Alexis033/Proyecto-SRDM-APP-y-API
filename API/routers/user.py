4f07c19b52b4cd2cdf9509d2d2ac2774a6ddc2a7
Alexis033 <10guevarafm1102@gmail.com>
2023-02-07 11:55:12 -0500

    cambio de nombre carpeta "router" por "routers"

--- API/routers/user.py
+++ API/routers/user.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter
 from schema.user_schema import UserSchema 
 
-user= APIRouter()
+user= APIRouter(prefix="/user",
+                tags=["user"])
 
-@user.get("/user")
+@user.get("/")
 def get_user():
     pass
 
-@user.post("/user")
+@user.post("/")
 def create_user(data_user: UserSchema):
     return data_user
 
-@user.put("/user")
+@user.put("/")
 def update_user(data_user: UserSchema):
     return data_user
